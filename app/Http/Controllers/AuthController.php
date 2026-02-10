9750d34cfa9a6f62015a9b7dfae41b58f5bcc4d8
Angel Castrillon <Angel.samwich@protonmail.com>
2026-02-09 20:43:08 -0500

    feat: implementar autenticación JWT y proteger rutas - Configurar JWT-Auth y el modelo User - Agregar AuthController con lógica de inicio de sesión/registro - Agregar middleware IsUserAuth - Proteger las rutas de productos en api.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    //Registro
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:250',
+            'email' => 'required|string|max:250|unique:users,email',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+        
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'error' => $validator->errors(),
+                'message' => 'Error al registrar el usuario'
+            ], 422);
+        }
+        User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password'))
+        ]);
+        return response()->json([
+            'success' => true,
+            'message' => 'Usuario registrado exitosamente'
+        ], 201);
+    }
+
+    //Login
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|string|email|max:250',
+            'password' => 'required|string|min:8'
+        ]);
+        
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'error' => $validator->errors(),
+                'message' => 'Error al registrar el usuario'
+            ], 422);
+        }
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)){
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Credenciales invalidas'
+                ], 401);
+            }
+            return response()->json([
+                'success' => true,
+                'token' => $token
+            ], 200);
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No se pudo crear el token',
+                'error' => $e->getMessage()
+            ], 500);
+            
+        }
+        
+
+    }
+
+    //Obtener usuario
+    public function getUser(){
+        $user = Auth::user();
+        return response()->json([
+            'success' => true,
+            'data' => $user
+        ], 200);
+    }
+
+    //Cerrar sesión
+    public function logout(){
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json([
+            'success' => true,
+            'message' => 'Se ha terminado la sesión exitosamente'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductCatalogController;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\IsUserAuth;
 
-Route::post('/products/catalog', [ProductCatalogController::class, 'index']);
-Route::get('/products/{id}', [ProductCatalogController::class, 'show']);
+//Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+
+//Private Routes
+Route::middleware(IsUserAuth::class)->group(function(){
+    Route::controller(AuthController::class)->group(function(){
+        Route::get('/user', 'getUser');
+        Route::post('/logout', 'logout');
+        Route::post('/products/catalog', [ProductCatalogController::class, 'index']);
+        Route::get('/products/{id}', [ProductCatalogController::class, 'show']);
+    });    
+});
